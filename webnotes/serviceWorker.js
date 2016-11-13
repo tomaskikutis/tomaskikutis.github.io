5a50a22b6579fff7c17dfcb53a0e9f22aff0d71f
Tomas Kikutis <t.kikutis@gmail.com>
2016-11-13 19:00:20 +0200

    Always return root url

--- webnotes/serviceWorker.js
+++ webnotes/serviceWorker.js
@@ -5,23 +5,23 @@ self.addEventListener('install', function(evt) {
    evt.waitUntil(precache());
 });
 self.addEventListener('fetch', function(evt) {
-  console.log('The service worker is serving the asset.');
-  evt.respondWith(fromCache(evt.request));
+  if (request.method === 'GET') {
+    console.log('The service worker is serving the asset.');
+    evt.respondWith(fromCache(evt.request));
+  }
   evt.waitUntil(update(evt.request));
 });
 
 function precache() {
   return caches.open(CACHE).then(function (cache) {
     return cache.addAll([
-      './'
+      '/'
     ]);
   });
 }
 function fromCache(request) {
   return caches.open(CACHE).then(function (cache) {
-    return cache.match(request).then(function (matching) {
-      return matching || Promise.reject('no-match');
-    });
+    return cache.match('/');
   });
 }
 function update(request) {
