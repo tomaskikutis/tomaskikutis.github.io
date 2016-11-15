b1a27c1b6aeb5d42ce3a630a2a60909ea6f205f9
Tomas Kikutis <t.kikutis@gmail.com>
2016-11-15 19:11:49 +0200

    Revert "Always return root url"
    This reverts commit 5a50a22b6579fff7c17dfcb53a0e9f22aff0d71f.

--- webnotes/serviceWorker.js
+++ webnotes/serviceWorker.js
@@ -5,23 +5,23 @@ self.addEventListener('install', function(evt) {
    evt.waitUntil(precache());
 });
 self.addEventListener('fetch', function(evt) {
-  if (request.method === 'GET') {
-    console.log('The service worker is serving the asset.');
-    evt.respondWith(fromCache(evt.request));
-  }
+  console.log('The service worker is serving the asset.');
+  evt.respondWith(fromCache(evt.request));
   evt.waitUntil(update(evt.request));
 });
 
 function precache() {
   return caches.open(CACHE).then(function (cache) {
     return cache.addAll([
-      '/'
+      './'
     ]);
   });
 }
 function fromCache(request) {
   return caches.open(CACHE).then(function (cache) {
-    return cache.match('/');
+    return cache.match(request).then(function (matching) {
+      return matching || Promise.reject('no-match');
+    });
   });
 }
 function update(request) {
