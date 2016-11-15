8a13150b882ff8642cd35ec8466ee133f6ecd50c
Tomas Kikutis <t.kikutis@gmail.com>
2016-11-15 19:09:24 +0200

    Revert "Fix AddAll"
    This reverts commit b9305d5c452d6ca78a9f683d9c203d0d42978f1f.

--- webnotes/serviceWorker.js
+++ webnotes/serviceWorker.js
@@ -15,7 +15,7 @@ self.addEventListener('fetch', function(evt) {
 function precache() {
   return caches.open(CACHE).then(function (cache) {
     return cache.addAll([
-      './'
+      '/'
     ]);
   });
 }
